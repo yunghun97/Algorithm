b412f990cde424abc82829084711a43b5b12dcbe
yunghun97 <yunghun97@naver.com>
2021-07-08 23:51:20 +0900

    commit

--- PromgrammersLv1Knum.java
+++ PromgrammersLv1Knum.java
@@ -4,7 +4,6 @@ public class PromgrammersLv1Knum {
         Solution Solve = new Solution();
         int array[] = {1,5,2,6,3,7,4};
         int commands[][] = {{2,5,3},{4,4,1},{1,7,3}};
-        
         System.out.print(Solve.solution(array, commands));
     }
 }
