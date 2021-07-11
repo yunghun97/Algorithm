2485a10aa1d1300083a050c6390269d59d0c2354
yunghun97 <yunghun97@naver.com>
2021-07-12 00:45:09 +0900

    commit

---
+++ Use JAVA/ProgrammersLv1PlusMinus.java
@@ -0,0 +1,23 @@
+public class ProgrammersLv1PlusMinus {
+    public static void main(String[] args){
+        int[] absolutes = {4,7,12};
+        boolean[] signs = {true, false, true};
+        Solution5 sol5 = new Solution5();
+        int answer = sol5.solution(absolutes, signs);
+        System.out.print(answer);
+    }
+}
+class Solution5 {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer =0;
+        for(int i=0; i<signs.length; i++){
+            if(signs[i]==true){
+                answer += absolutes[i];
+            }
+            else{
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
