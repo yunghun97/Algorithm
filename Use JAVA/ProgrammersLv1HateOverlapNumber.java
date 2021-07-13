2b092efd9d11e2e68b4e88576040f2bc9e3fe2bf
yunghun97 <yunghun97@naver.com>
2021-07-13 21:54:34 +0900

    commit

---
+++ Use JAVA/ProgrammersLv1HateOverlapNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ProgrammersLv1HateOverlapNumber {
+    public static void main(String[] args){
+            int[] Number = {1,1,3,3,0,1,1}; 
+            Solution8 Sol8 = new Solution8();
+            int[] output;
+            output = Sol8.solution8(Number);
+            System.out.println(Arrays.toString(output));
+
+
+    }
+}
+class Solution8 {
+    public int[] solution8(int []arr) {
+        ArrayList<Integer> temp = new ArrayList<Integer>();
+        int input = 10;
+        for(int num : arr){
+            if(input!=num)
+                temp.add(num);
+            input = num;
+        }
+        
+        int[] answer = new int[temp.size()];
+        for(int i =0; i<answer.length; i++){
+            answer[i] = temp.get(i).intValue();
+        }
+        return answer;
+    }
+}
