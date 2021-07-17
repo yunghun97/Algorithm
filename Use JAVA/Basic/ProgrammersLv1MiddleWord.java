d24e6ab89414361445f19467af3813bfb47f4892
yunghun97 <yunghun97@naver.com>
2021-07-17 20:59:49 +0900

    commit

---
+++ Use JAVA/Basic/ProgrammersLv1MiddleWord.java
@@ -0,0 +1,17 @@
+public class ProgrammersLv1MiddleWord {
+    public static void main(String[] args) {
+        // String s = "abcde";
+        String s = "1a2a3a4a";
+        int a = s.length();
+        int i = s.length()/2;
+        String answer;
+        if(a%2==0){
+            answer = s.substring(i-1,i+1);
+        }
+        else{
+            answer = s.substring(i,i+1);
+        }
+        System.out.println(answer);
+    }
+    
+}
