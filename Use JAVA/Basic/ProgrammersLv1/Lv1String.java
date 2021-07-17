82a5c6a818db196e1aeccc9d454d2d1bf5d24648
yunghun97 <yunghun97@naver.com>
2021-07-17 22:53:15 +0900

    commit

---
+++ Use JAVA/Basic/ProgrammersLv1/Lv1String.java
@@ -0,0 +1,19 @@
+public class Lv1String {
+    public static void main(String[] args) {
+        String s= "1a654";
+        boolean answer = true;
+        if(s.length()==4||s.length()==6){
+        try{int a = Integer.parseInt(s);
+        }catch(NumberFormatException e){
+            answer = false;
+            throw e;
+        }finally{
+            System.out.println(answer);
+        }
+        }
+        else{
+            answer = false;
+            System.out.println(answer);
+        }
+    }
+}
