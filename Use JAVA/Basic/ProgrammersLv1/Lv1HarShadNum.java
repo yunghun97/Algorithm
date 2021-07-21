ed95a835269b48b82d6184fc8ae17fe593af70e0
yunghun97 <yunghun97@naver.com>
2021-07-22 01:39:01 +0900

    commit

---
+++ Use JAVA/Basic/ProgrammersLv1/Lv1HarShadNum.java
@@ -0,0 +1,20 @@
+package Basic.ProgrammersLv1;
+
+public class Lv1HarShadNum {
+    public static void main(String[] args) {
+        
+    
+    int x = 13;
+    boolean answer = false;
+    int nSum =0;
+    int input = x;
+    while(input>0){
+        nSum += input%10;
+        input/=10;
+    }
+    if(x%nSum==0){
+        answer = true;
+    }
+    System.out.println(answer);
+    }
+}
