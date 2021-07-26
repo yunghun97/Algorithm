2073286b722c5fe6abaa6083194fc5caf2977e0c
yunghun97 <yunghun97@naver.com>
2021-07-26 21:27:42 +0900

    commit

---
+++ Use JAVA/SamSungSwExpert/D2/NumberSort.java
@@ -0,0 +1,27 @@
+package SamSungSwExpert.D2;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class NumberSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int t=1; t<=T; t++){
+            int num = sc.nextInt();
+        	sc.nextLine();
+        	String input = sc.nextLine();
+            String[] arr = new String[num];
+            int[] answer = new int[num];
+            arr = input.split(" ");
+            for(int i=0; i<arr.length; i++){
+            	answer[i] = Integer.parseInt(arr[i]);
+            }
+            Arrays.sort(answer);
+            System.out.printf("#%d ",t);
+            for(int a : answer){
+            	System.out.printf("%d ",a);
+            }
+            System.out.println("");
+        }
+    }
+}
