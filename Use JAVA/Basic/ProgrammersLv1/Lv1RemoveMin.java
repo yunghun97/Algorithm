e1942a25aa8d7d0bba95b7fefc90aa990059a4d5
yunghun97 <yunghun97@naver.com>
2021-07-26 22:08:59 +0900

    commit

---
+++ Use JAVA/Basic/ProgrammersLv1/Lv1RemoveMin.java
@@ -0,0 +1,36 @@
+package Basic.ProgrammersLv1;
+
+import java.util.Arrays;
+
+public class Lv1RemoveMin {
+    public static void main(String[] args) {
+    int[] arr ={5,5,5,5,1,3,0,1};
+    int[] zero = {-1};
+        int first = arr.length;
+        int MIN = 1000000;
+        int map = 0;
+        int a=0;
+        int[] answer = new int[first-1];
+        if(first==1){
+            System.out.println(Arrays.toString(zero));
+        }
+        else{
+            for(int i=0; i<first; i++){
+                if(arr[i]<MIN){
+                    map = i;
+                    MIN = arr[i];
+                }
+            }
+            for(int j=0; j<first; j++){
+                if(j==map){
+                    continue;
+                }
+                else{
+                    answer[a]=arr[j];
+                    a++;
+                }
+            }
+        }
+            System.out.println(Arrays.toString(answer));
+    }
+}
