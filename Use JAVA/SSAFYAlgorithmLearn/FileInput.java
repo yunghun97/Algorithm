333834de7401b5dc0e8d6df0824ad56c42b6f6ab
yunghun97 <yunghun97@naver.com>
2021-08-02 12:30:40 +0900

    commit

--- Use JAVA/SSAFYAlgorithmLearn/FileInput.java
+++ Use JAVA/SSAFYAlgorithmLearn/FileInput.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 
 public class FileInput {
     public static void main(String[] args) throws FileNotFoundException {
-        System.setIn(new FileInputStream("input.txt"));
+        System.setIn(new FileInputStream("Use JAVA\\SSAFYAlgorithmLearn\\input.txt"));
         Scanner sc = new Scanner(System.in);
+        System.out.printf(sc.nextLine());
     }
 }
