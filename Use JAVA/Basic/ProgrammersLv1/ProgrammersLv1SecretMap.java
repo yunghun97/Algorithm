b970ff0ee20ea624f5c8364b97269e31b4d7d26a
yunghun97 <yunghun97@naver.com>
2021-08-07 01:36:52 +0900

    commit

--- Use JAVA/Basic/ProgrammersLv1/ProgrammersLv1SecretMap.java
+++ Use JAVA/Basic/ProgrammersLv1/ProgrammersLv1SecretMap.java
@@ -7,7 +7,7 @@ public class ProgrammersLv1SecretMap{
         String[] user = new String[n];
         for(int i =0; i<n; i++){
         user[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-        user[i] = String.format("%"+n+"s",user[i]);
+        user[i] = String.format("%"+n+"s",user[i]); // 앞에 자리가 0일경우 생략하게 되므로 강제로 추가 해준다.
         user[i] = user[i].replace("1", "#");
         user[i] = user[i].replace("0", " ");
         }
