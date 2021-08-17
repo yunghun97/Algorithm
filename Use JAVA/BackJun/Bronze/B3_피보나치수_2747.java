bb44f37f2eee2f1e7efe85af846ac9b4f5961615
yunghun97 <yunghun97@naver.com>
2021-08-18 00:08:51 +0900

    coomit

---
+++ Use JAVA/BackJun/Bronze/B3_피보나치수_2747.java
@@ -0,0 +1,22 @@
+package BackJun.Bronze;
+import java.io.*;
+public class B3_피보나치수_2747 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = Integer.parseInt(br.readLine());
+        long arr[] = new long[input+1];
+        arr[0]=0;
+        arr[1]=1;
+        for(int i=2; i<input+1; i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        long answer = arr[input];
+        bw.write(""+answer);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
+
+//https://www.acmicpc.net/problem/1212
