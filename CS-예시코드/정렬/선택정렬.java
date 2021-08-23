272447e128657d2632b56104445ac96ef05997c5
yunghun97 <yunghun97@naver.com>
2021-08-24 00:45:34 +0900

    commit

--- CS-예시코드/정렬/선택정렬.java
+++ CS-예시코드/정렬/선택정렬.java
@@ -1,6 +1,6 @@
 package 정렬;
 
-import java.util.Arrays;
+
 
 public class 선택정렬 { // 선택정렬  주어진 리스트에서 최솟값을 찾는다. -> 최솟값을 맨 앞 자리의 값과 교환한다. -> 교환한자리 제외하고 그 다음자리부터 마지막까지 반복
     public static void main(String[] args) {
@@ -15,11 +15,11 @@ public class 선택정렬 { // 선택정렬  주어진 리스트에서 최솟값
             }
             swap(arr, min_index, i); // 배열, 최소 인덱스(j에서 최소값을 찾지 못하면 i값과 i를 스왑하므로 결과는 똑같아 진다.), i를 매개변수로 전달
         }
-        System.out.println(Arrays.toString(arr));
     }
     static void swap(int[] arr, int i, int j){
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
+    
 }
