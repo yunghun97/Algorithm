601afa8e5c22cc55358cf4cb68b0211adc8a3e45
yunghun97 <yunghun97@naver.com>
2021-09-08 02:24:41 +0900

    0908

--- Use JAVA/BackJun/Silver/S3다음순열_10972.java
+++ Use JAVA/BackJun/Silver/S3다음순열_10972.java
@@ -19,23 +19,23 @@ public class S3다음순열_10972 {
         int index1 = -1, index2 = 0;
         for(int i=N-1; i>=1; i--){
             if(arr[i]>arr[i-1]){
-                index1 = i-1;
+                index1 = i-1;   // 내림차순 아닌 부분 찾기
                 break;
             }
         }
-        if(index1==-1){
+        if(index1==-1){ // 모두 내림차순 이므로 -1
             bw.write("-1");
         }else{
             for(int i=N-1; i>=0; i--){
-                if(arr[index1]<arr[i]){
+                if(arr[index1]<arr[i]){ // 내림차순이 발생하는 가장 큰 자릿수의 인덱스 보다 더 큰 자리를 찾는다
                     index2=i;
                     break;
                 }
             }
-            int temp = arr[index1];
+            int temp = arr[index1];     // swap
             arr[index1] = arr[index2];
             arr[index2] = temp;
-            Arrays.sort(arr, index1+1, N);
+            Arrays.sort(arr, index1+1, N);  // index+1부터 N전까지 정렬
             for(int i=0; i<N; i++){
                 bw.write(""+arr[i]+" ");
             }
@@ -46,3 +46,4 @@ public class S3다음순열_10972 {
         br.close();
     }
 }
+//https://www.acmicpc.net/problem/10972
