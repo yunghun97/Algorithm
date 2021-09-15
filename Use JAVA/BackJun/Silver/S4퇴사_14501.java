806965a1069f469e299f3dc6060f168718600994
yunghun97 <yunghun97@naver.com>
2021-09-16 01:38:14 +0900

    0916

--- Use JAVA/BackJun/Silver/S4퇴사_14501.java
+++ Use JAVA/BackJun/Silver/S4퇴사_14501.java
@@ -8,30 +8,25 @@ public class S4퇴사_14501 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
         int size = Integer.parseInt(br.readLine());
-        int[] day = new int[size+1];
-        int[] score = new int[size+1];
+        int[] day = new int[size+2];    // 트래쉬값 1개 추가
+        int[] score = new int[size+2];
+        int[] dp = new int[size+2]; // size+1 까지 정답이므로
+        int max =0;
         for(int i=1; i<=size; i++){  
             st = new StringTokenizer(br.readLine());
             day[i] = Integer.parseInt(st.nextToken());
             score[i] = Integer.parseInt(st.nextToken());
         }
-        // System.out.println(Arrays.toString(score));
-        // System.out.println(Arrays.toString(day));
-        int[][] dp = new int[size+1][size+1];
-
-        for(int i=1; i<=size; i++){
-            for(int j=1; j<=size; j++){
-                if(day[i]+i-1<=j){
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-day[i]]+score[i]);
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-            System.out.println(Arrays.deepToString(dp));
-        }
         
-        int answer = dp[size][size];
-        bw.write(""+answer);
+        for(int i=1; i<=size+1; i++){
+            dp[i] = Math.max(dp[i],max);    // 현재 좌표랑 좌표 오기전까지의 MAX 값 비교
+            if(i+day[i]<=size+1){
+                dp[day[i]+i] = Math.max(dp[day[i]+i],score[i]+dp[i]);   // 상담 후 금액 = Max(상담 안 했지만 상담 했을 때 위치하는 금액 vs 내가 상담한 금액(현재 금액+상담 금))
+            }
+            max = Math.max(max,dp[i]);  // 현재 일에서 최대 값 저장
+        }
+        bw.write(""+max);
         bw.flush();
     }
 }
+// https://www.acmicpc.net/problem/14501
