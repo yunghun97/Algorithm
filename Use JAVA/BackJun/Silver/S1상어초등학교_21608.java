cab623ee67f449bb58061abe8853416413df29bc
yunghun97 <yunghun97@naver.com>
2021-09-16 12:03:55 +0900

    0916

--- Use JAVA/BackJun/Silver/S1상어초등학교_21608.java
+++ Use JAVA/BackJun/Silver/S1상어초등학교_21608.java
@@ -6,7 +6,7 @@ import java.util.*;
 1을 만족하는 칸이 여러 개이면, 인접한 칸 중에서 비어있는 칸이 가장 많은 칸으로 자리를 정한다.
 2를 만족하는 칸도 여러 개인 경우에는 행의 번호가 가장 작은 칸으로, 그러한 칸도 여러 개이면 열의 번호가 가장 작은 칸으로 자리를 정한다.
 */
-public class S1상어초등학교_21608_copy { 
+public class S1상어초등학교_21608 { 
     static int[][] map, likeArr;
     static int N,answer;
     static ArrayList<Student> list;
