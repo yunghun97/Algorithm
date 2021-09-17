afdf49a3446020ed43dcae201b5ffe2dbbbf893b
yunghun97 <yunghun97@naver.com>
2021-09-17 09:15:32 +0900

    0917

--- Use JAVA/SSFAlgorithmLearn/Day11/BinaryTest.java
+++ Use JAVA/SSFAlgorithmLearn/Day11/BinaryTest.java
@@ -2,15 +2,15 @@ package SSFAlgorithmLearn.Day11;
 import java.util.Arrays;
 public class BinaryTest {
     public static void main(String[] args) {
-        int[] values = {3,11,15,20,21,45} ;
+        int[] values = {3,7,11,11,15,20,21,45} ;
         System.out.println(Arrays.binarySearch(values, 3));
-        System.out.println(Arrays.binarySearch(values, 45));
         System.out.println(Arrays.binarySearch(values, 11));
-        System.out.println(Arrays.binarySearch(values, 30));
-        System.out.println(Arrays.binarySearch(values, 1));
-        System.out.println(Arrays.binarySearch(values, 50));
+        // System.out.println(Arrays.binarySearch(values, 11));
+        // System.out.println(Arrays.binarySearch(values, 30));
+        // System.out.println(Arrays.binarySearch(values, 1));
+        // System.out.println(Arrays.binarySearch(values, 50));
 
-        System.out.println(Arrays.binarySearch(values, 1, 4, 45));
-        System.out.println(Arrays.binarySearch(values, 0, 0, 45));
+        // System.out.println(Arrays.binarySearch(values, 1, 4, 45));
+        // System.out.println(Arrays.binarySearch(values, 0, 0, 45));
     }
 }
