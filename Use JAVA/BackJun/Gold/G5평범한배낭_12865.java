ad35f6daf18674365d7cfeee7ffcaee16a746a4d
yunghun97 <yunghun97@naver.com>
2021-09-27 00:18:18 +0900

    0927

--- Use JAVA/BackJun/Gold/G5평범한배낭_12865.java
+++ Use JAVA/BackJun/Gold/G5평범한배낭_12865.java
@@ -7,31 +7,28 @@ public class G5평범한배낭_12865 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
-        
         st = new StringTokenizer(br.readLine());
         int count = Integer.parseInt(st.nextToken());
         int weight = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[count+1][weight+1];
-
-        for(int i=1; i<=count; i++){
+        int w =0;
+        int score = 0;
+        int[] dp = new int[weight+1];
+        for(int i=0; i<count; i++){
             st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int score = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            score= Integer.parseInt(st.nextToken());
 
-            for(int j=0; j<=weight; j++){
-                if(w<=j){   // 가방에 물품을 넣기 가능
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i-1][j-w]+score);
-
-                }else{ // 불가능
-                    dp[i][j] = dp[i-1][j];
-                }
+            for(int j=weight; j>=1; j--){
+                if(j>=w) dp[j] = Math.max(dp[j],dp[j-w]+score);
+                else continue;
             }
-
         }
-        bw.write(""+dp[count][weight]);
+
+    
+        bw.write(""+dp[weight]);
         bw.flush();
-        bw.close();
         br.close();
+        bw.close();
     }
 }
 //https://www.acmicpc.net/problem/12865
