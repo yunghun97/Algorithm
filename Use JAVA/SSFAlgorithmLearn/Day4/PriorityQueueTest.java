c1934e4cb03ecdfa9e61795288981351d59eaed9
yunghun97 <yunghun97@naver.com>
2021-09-26 21:02:20 +0900

    0926

--- Use JAVA/SSFAlgorithmLearn/Day4/PriorityQueueTest.java
+++ Use JAVA/SSFAlgorithmLearn/Day4/PriorityQueueTest.java
@@ -1,4 +1,5 @@
 package SSFAlgorithmLearn.Day4;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 public class PriorityQueueTest {
 	public static void main(String[] args) {
@@ -12,8 +13,21 @@ public class PriorityQueueTest {
 			}
 		});*/
 		//PriorityQueue<Student> q = new PriorityQueue<>((o1, o2)-> o1.score - o2.score);
-		//@FunctionalInterface
+		
+		
+		//@FunctionalInterface  -> 메소드가 하나인 인터페이스는 람다함수 사용 가능
+
 		PriorityQueue<Student> q = new PriorityQueue<>((o1, o2)-> Integer.compare(o1.score, o2.score));
+
+		// 원래
+		PriorityQueue<Student> pq = new PriorityQueue<>(new Comparator<Student>(){
+			public int compare(Student o1, Student o2){
+				return o1.no-o2.no;
+			}
+		});
+
+		// Comparable<클래스명> = public int compareTo(클래스명 변수)
+		// Comparator<클래스명> = public int compare(클래스명 변수, 클래스명 변수)
 		q.offer(new Student(5,10));
 		q.offer(new Student(1,70));
 		q.offer(new Student(2,40));
