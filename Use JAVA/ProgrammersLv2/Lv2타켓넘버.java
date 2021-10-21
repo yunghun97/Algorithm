be03456bfcf4059c157299958a76a4a2dc54bec3
yunghun97 <yunghun97@naver.com>
2021-10-22 01:06:16 +0900

    1021

---
+++ Use JAVA/ProgrammersLv2/Lv2타켓넘버.java
@@ -0,0 +1,25 @@
+package ProgrammersLv2;
+
+public class Lv2타켓넘버 {
+    /*
+    class Solution {
+    static int answer, number;
+    static int[] arr;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        number = target;
+        arr = numbers;
+        DFS(0,0);
+        return answer;
+    }
+    public void DFS(int cnt, int result){
+        if(cnt==arr.length){
+            if(result==number) answer++;
+            return;
+        }
+            DFS(cnt+1, result+arr[cnt]);
+            DFS(cnt+1, result-arr[cnt]);
+    }
+}
+    */
+}
