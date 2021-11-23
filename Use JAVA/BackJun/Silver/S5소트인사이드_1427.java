92448a964ff17e1dabed9091d35fada630811a25
권영현 <yunghun97@naver.com>
2021-11-23 18:05:50 +0900

    1123

---
+++ Use JAVA/BackJun/Silver/S5소트인사이드_1427.java
@@ -0,0 +1,23 @@
+package BackJun.Silver;
+
+import java.io.*;
+import java.util.*;
+public class S5소트인사이드_1427 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        char[] arr = br.readLine().toCharArray();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>((o1,o2)->{
+            return Integer.compare(o2, o1);
+        });
+        for(int i=0; i<arr.length; i++){
+            pq.add((int)(arr[i]-48));
+        }
+        StringBuffer sb = new StringBuffer();
+        while(!pq.isEmpty()){
+            sb.append(pq.poll());
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
