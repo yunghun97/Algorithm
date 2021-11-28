4370dc164311d8dbcbd6dc16c5f6c7b329f44f78
권영현 <yunghun97@naver.com>
2021-11-28 19:20:50 +0900

    1128

---
+++ Use JAVA/BackJun/Gold/G1온풍기안녕_23289.java
@@ -0,0 +1,15 @@
+package BackJun.Gold;
+
+import java.io.*;
+import java.util.*;
+
+public class G1온풍기안녕_23289 {
+    static int map[][];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+    } 
+}
