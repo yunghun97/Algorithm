937252291dff0fa5dcdb91bbac01975fbe9f172d
권영현 <yunghun97@naver.com>
2021-12-10 22:15:47 +0900

    1210

---
+++ Use JAVA/SamSungSwExpert/모의역랑테스트/보물상자비밀번호_5658.java
@@ -0,0 +1,11 @@
+package SamSungSwExpert.모의역랑테스트;
+
+import java.io.*;
+import java.util.*;
+public class 보물상자비밀번호_5658 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+    }
+}
