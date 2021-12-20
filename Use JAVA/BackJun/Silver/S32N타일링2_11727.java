067a8c2938bc3b1430b50ca0148b4d025c6ac182
권영현 <yunghun97@naver.com>
2021-12-20 23:23:27 +0900

    1220

--- Use JAVA/BackJun/Silver/S32N타일링2_11727.java
+++ Use JAVA/BackJun/Silver/S32N타일링2_11727.java
@@ -9,9 +9,8 @@ public class S32N타일링2_11727 {
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+1];
         dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 3;
-        for (int i = 3; i <= N; i++) {
+        dp[1] = 1;        
+        for (int i = 2; i <= N; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
         }
 
