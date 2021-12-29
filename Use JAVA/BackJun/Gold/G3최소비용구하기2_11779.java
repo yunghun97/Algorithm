0d49159cec85920b4801b2c947f3c172e6ce47a6
권영현 <yunghun97@naver.com>
2021-12-29 19:08:35 +0900

    1129

---
+++ Use JAVA/BackJun/Gold/G3최소비용구하기2_11779.java
@@ -0,0 +1,13 @@
+package BackJun.Gold;
+
+import java.io.*;
+import java.util.*;
+
+public class G3최소비용구하기2_11779 {
+    public static void main(String[] args) throws IOException{
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+         StringTokenizer st;
+         
+    }    
+}
