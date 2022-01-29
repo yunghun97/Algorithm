5302b943578295fc8703c295e016ef23b7ee3064
gwon <yunghun97@naver.com>
2022-01-29 20:15:11 +0900

    22.01.29

---
+++ Use JAVA/SamSungSwExpert/D5/범준이의제주도여행계획_1798.java
@@ -0,0 +1,21 @@
+package SamSungSwExpert.D5;
+
+import java.io.*;
+import java.util.*;
+public class 범준이의제주도여행계획_1798 {
+    static int N, M;
+    // 9시간을 넘게 소요하면 안된다.
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++){
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+
+        }// 테케 끝
+    }
+}
