04bee203381080329aef284d54c13870f0ff6cfe
gwon <yunghun97@naver.com>
2022-02-01 02:20:41 +0900

    22.02.01

--- Use JAVA/BackJun/Gold/G4캐슬디펜스_17135.java
+++ Use JAVA/BackJun/Gold/G4캐슬디펜스_17135.java
@@ -4,130 +4,141 @@ import java.util.*;
 
 
 public class G4캐슬디펜스_17135 {
-    static int answer, temp, range, N, M, distance, destroy;
-    static int[][] map, mapTemp, mapClone;
+    static int R, C, D;
+    static int[][] map, defalutMap;
     static int[] bow;
-        public static void main(String[] args) throws IOException {
-            System.setIn(new FileInputStream("Use JAVA\\inputTxt모음\\BackJun\\Gold\\G4캐슬디펜스_17135.txt"));
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-            StringTokenizer st;
+    static int answer;
+    static PriorityQueue<Node> pq1, pq2, pq3;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
+        map = new int[R][C];
+        defalutMap = new int[R][C];
+        bow = new int[3];
+        answer = 0;
+        for(int i=0; i<R; i++){
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            M = Integer.parseInt(st.nextToken());
-            range = Integer.parseInt(st.nextToken());
-            map = new int[N][M];
-            mapClone = new int[N][M];
-            bow = new int[3];
-            mapTemp = new int[range][M];
-            distance =0;
-            destroy =0;
-            for(int i=N-1; i>=0; i--){
-                st = new StringTokenizer(br.readLine());
-                for(int j=0; j<M; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            combination(0,0);
-            System.out.println(answer);
-        }
-        
-        private static void combination(int cnt, int start) {
-            if(cnt==3){
-                cal();       //bow배열의 값은 궁수의 column 좌표가 된다.
-                answer = Math.max(destroy, answer);
-                destroy =0;
-                return;
-            }
-            for(int i=start; i<M; i++){
-                bow[cnt] = i;
-                combination(cnt+1, i+1);
+            for(int j=0; j<C; j++){
+                defalutMap[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
-        
-        
-        private static void cal() { // 궁수의 좌표
-            for(int i=0; i<N; i++){
-                for(int j=0; j<M; j++){
-                    mapClone[i][j] = map[i][j];
-                }
-            }
-            boolean[][] overLapCheck = new boolean[N][M];
-            
-
-            for(int start0=0; start0<N; start0++){
-                int[][] rc = new int[3][2];
-
-            for(int b=0; b<3; b++){
-                boolean enemyCheck = false;
-                int checkMin = Integer.MAX_VALUE;   
-                int r=0; int c=0;
-            for(int start1 =start0; start1<start0+range; start1++){
-                if(enemyCheck) break;
-                enemyCheck = false;
-                if(start1<N){
-
-                for(int start2=M-1; start2>=0; start2--){
-                    if(mapClone[start1][start2]==1){
-                        enemyCheck = true;
-                        int temp = Math.min(checkMin,Math.abs(start2-bow[b]));
-                        if(temp<=checkMin){
-                            r = start1; c=start2;
-                            checkMin = temp;
+        pq1 = new PriorityQueue<Node>();
+        pq2 = new PriorityQueue<Node>();
+        pq3 = new PriorityQueue<Node>();
+        combination(0,0);
+        bw.write(""+answer);
+        bw.flush();
+    }
+    private static void combination(int cnt, int start) {
+        if(cnt==3){
+            for(int i=0; i<R; i++) System.arraycopy(defalutMap[i], 0, map[i], 0, C);    // 매 게임마다 배열 초기화
+            gameStart();
+            return;
+        }
+        for(int i=start; i<C; i++){
+            bow[cnt] = i;
+            combination(cnt+1, i+1);
+        }
+    }
+    
+    private static void gameStart() {   // 게임 시작
+        int result = 0;
+        while (true) {
+            for (int r = R - 1; r >= 0; r--) {
+                for (int c = 0; c < C; c++) {
+                    if (map[r][c] == 1) { // 적이 있는 경우
+                        if (Math.abs(r - R) + Math.abs(c - bow[0]) <= D) {
+                            pq1.add(new Node(r, c, Math.abs(r - R) + Math.abs(c - bow[0])));    // 첫번 쨰 궁수
+                        }
+                        if (Math.abs(r - R) + Math.abs(c - bow[1]) <= D) {
+                            pq2.add(new Node(r, c, Math.abs(r - R) + Math.abs(c - bow[1])));    // 두번 째 궁수
+                        }
+                        if (Math.abs(r - R) + Math.abs(c - bow[2]) <= D) {
+                            pq3.add(new Node(r, c, Math.abs(r - R) + Math.abs(c - bow[2])));    // 3번 째 궁수
                         }
                     }
                 }
             }
-                else break;
-            }
-                if(!overLapCheck[r][c]){
-                    ++destroy; 
-                    overLapCheck[r][c]=true;
-                    mapClone[r][c]=0;
-                    rc[b][0] = r; rc[b][1]=c;
+            int tmpR = -1; int tmpC = -1;   // 기존의 좌표와 겹치는지 확인용
+            if(!pq1.isEmpty()){ // 첫번 쨰 궁수
+                Node node = pq1.poll();
+                map[node.r][node.c] = 0;    // 적 없애기
+                if(node.r!=tmpR||node.c!=tmpC){ // 겹치지 않는 경우 좌표 새로 할당 및 answer++;
+                    tmpR = node.r;
+                    tmpC = node.c;
+                    result++;
                 }
-
             }
-            for(int rc1=0; rc1<3; rc1++){mapClone[rc[rc1][0]][rc[rc1][1]] =0;}
+            if(!pq2.isEmpty()){ //두번 쨰 궁수
+                Node node = pq2.poll();
+                map[node.r][node.c] = 0; // 적 없애기
+                if(node.r!=tmpR||node.c!=tmpC){ // 겹치지 않는 경우 좌표 새로 할당 및 answer++;
+                    tmpR = node.r;
+                    tmpC = node.c;
+                    result++;
+                }
+            }
+            if(!pq3.isEmpty()){ //3번 째 궁수
+                Node node = pq3.poll();
+                map[node.r][node.c] = 0; // 적 없애기
+                if(node.r!=tmpR||node.c!=tmpC){ // 겹치지 않는 경우 좌표 새로 할당 및 answer++;
+                    tmpR = node.r;
+                    tmpC = node.c;
+                    result++;
+                }
+            }
+            pq1.clear();
+            pq2.clear();
+            pq3.clear();
+            enemyMove();    // 적 움직이기
+            if(endCheck()){ // 끝났는지 체크
+                answer = Math.max(result, answer);
+                return;
+            }
         }
+    }
+    private static boolean endCheck() {
+        for(int r=0; r<R; r++){
+            for(int c=0; c<C; c++){
+                if(map[r][c]==1) return false;
+            }
+        }
+        return true;
+    }
+    private static void enemyMove() {
+        
+        for (int r = R-1; r >= 1; r--) {
+            for (int c = 0; c < C; c++) {
+                map[r][c] = map[r-1][c];
+            }
+        }
+        for(int x=0; x<C; x++) map[0][x] = 0;
 
+    }
+    static class Node implements Comparable<Node>{
+        int r;
+        int c;
+        int weight;
+        public Node(int r, int c, int weight){
+            this.r = r;
+            this.c = c;
+            this.weight = weight;
         }
-}
     
-
-
-
-        /*private static void kill() {
-            boolean[][] overLapCheck = new boolean[mapTemp.length][mapTemp[0].length];
-           
-                for(int i=0; i<mapTemp.length;i++){
-                    for(int y=0; y<mapTemp[i].length; y++){
-                        System.out.print(mapTemp[i][y]+" ");
-                    }
-                    System.out.println();
-        }
-
-        }*/
-
-
-/*int cal = Integer.MAX_VALUE;
-                 for(int b1=0; b1<3; b1++){     // 궁수 인원 만큼
-                int locateC =0; int locateR=0;
-                for(int r1=0; r1<range; r1++){
-                for(int b2=0; b2<mapTemp[0].length; b2++){
-                    for(int b3=0; b3<mapTemp.length; b3++){
-                        if(mapTemp[b3][b2]==1){
-                        if(cal<=Math.abs(b2-bow[b1])){
-                        cal = Math.min(Math.abs(b2-bow[b1]), cal);
-                        locateC = b3; locateR = b2;
-                    }}
-
-                    }
-                }
-                if(!overLapCheck[locateR][locateC]){ 
-                    mapClone[locateR][locateC]=0;
-                    destroy++; overLapCheck[locateR][locateC]=true;}
-                else{}
+        @Override
+        public int compareTo(Node o) {
+            if(this.weight!=o.weight){
+                return Integer.compare(this.weight, o.weight);
+            }else{
+                return Integer.compare(this.c, o.c);
             }
-        }*/
+        }
+    
+    }
+}
+// https://www.acmicpc.net/problem/17135
