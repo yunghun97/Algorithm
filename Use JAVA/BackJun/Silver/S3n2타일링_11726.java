4d558aff59693bda96c0aa81d2db3615167b05ee
GWON97 <yunghun97@naver.com>
2022-05-19 19:55:36 +0900

    22.05.19

--- Use JAVA/BackJun/Silver/S3n2타일링_11726.java
+++ Use JAVA/BackJun/Silver/S3n2타일링_11726.java
@@ -11,15 +11,14 @@ public class S3n2타일링_11726 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
+        int[] arr = new int[n+2];
         arr[1] = 1;
         arr[2] = 2;
-        arr[3] = 3;
         int answer = 0;
-        if(n<=3){
+        if(n<=2){
             answer = arr[n];
         }else{
-            for(int i=4; i<=n; i++){
+            for(int i=3; i<=n; i++){
                 arr[i] = (arr[i-1]+arr[i-2]) % 10007;
             }
             answer = arr[n];
